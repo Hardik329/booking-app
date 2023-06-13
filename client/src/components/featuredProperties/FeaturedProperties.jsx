462c23992df3ce9cf478d8da128bde583f322ad2
hardik <hardik32904@gmail.com>
2023-06-14 00:45:02 +0530

    Fixed url and query

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -3,29 +3,31 @@ import useFetch from "../../hooks/useFetch";
 import "./FeaturedProperties.css";
 
 function FeaturedProperties() {
-  const {data, loading, error} = useFetch("/hotels/?featured=0&limit=4");
+  const { data, loading, error } = useFetch("/hotels?featured=0&limit=4");
   return (
     <div className="fp">
       {loading ? (
         "Loading..."
       ) : (
         <>
-          {data.map((item)=>(
-          <div className="fpItem" key={item._id}>
-            <img
-              src="https://www.tajhotels.com/content/dam/tajhotels/ihcl/vivanta/vivanta_sikkim_ihcl.jpg"
-              alt=""
-              className="fpImg"
-            />
-            <span className="fpName">{item.name}</span>
-            <span className="fpCity">{item.city}</span>
-            <span className="fpPrice">{item.cheapestPrice}</span>
-            {item.rating && <div className="fpRating">
-              <button>{item.rating}</button>
-              <span>Excellent</span>
-            </div>}
-          </div>
-        ))}
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              <img
+                src="https://www.tajhotels.com/content/dam/tajhotels/ihcl/vivanta/vivanta_sikkim_ihcl.jpg"
+                alt=""
+                className="fpImg"
+              />
+              <span className="fpName">{item.name}</span>
+              <span className="fpCity">{item.city}</span>
+              <span className="fpPrice">{item.cheapestPrice}</span>
+              {item.rating && (
+                <div className="fpRating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
+            </div>
+          ))}
         </>
       )}
     </div>
